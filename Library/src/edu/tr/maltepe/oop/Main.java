1dc2e16ac8bbebb6d8891bdc86c599a288feac33
bozgen <bilalozgen00@gmail.com>
2022-03-28 23:28:03 +0300

    clear comments

--- Library/src/edu/tr/maltepe/oop/Main.java
+++ Library/src/edu/tr/maltepe/oop/Main.java
@@ -2,31 +2,6 @@ package edu.tr.maltepe.oop;
 
 public class Main {
     public static void main(String[] args){
-        /*  PREVIOUS LAB
-        Student student1 = new Student("bilal", "ozgen", 22, 6548);
-        Student student2 = new Student("baran", "yeni", 20, 6549);
-
-        Professor professor1 = new Professor("ensar", "gul", 50,"oop");
-        Professor professor2 = new Professor("kayhan", "erciyes", 50, "digital system design");
-
-        System.out.println("Student " + student1.getName()+ " with id "+ student1.getId() + " is " + student1.getAge() + " years old.");
-        System.out.println("Student " + student2.getName()+ " with id "+ student2.getId() + " is " + student2.getAge() + " years old.");
-
-        System.out.println("Professor " + professor1.getName()+ " who teaches "+ professor1.getLecture()+ " is "+ professor1.getAge() + " years old.");
-        System.out.println("Professor " + professor2.getName()+ " who teaches "+ professor2.getLecture()+ " is "+ professor2.getAge() + " years old.");
-
-        System.out.println("\n");
-
-        professor1.setLecture("Object Oriented Programming");
-        System.out.println("Professor " + professor1.getName()+ " who teaches "+ professor1.getLecture()+ " is "+ professor1.getAge() + " years old.");
-
-
-        professor2.tellLecture();
-
-        student2.greet();
-        */
-
-        /* Assignment  */
         Student s1 = new Student("Bilal","Ozgen",22,6548);
         Professor p1 = new Professor("Ensar", "Gul", 60, "oop");
         Library lib = new Library("Maltepe University Library");
